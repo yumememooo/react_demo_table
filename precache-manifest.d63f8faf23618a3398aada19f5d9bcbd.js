c87506b4bc08fdc0cf93b1761dc3bdaa65201891
yumememooo <yumememooo@gmail.com>
2022-10-15 16:10:45 +0800

    Updates

---
+++ precache-manifest.d63f8faf23618a3398aada19f5d9bcbd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1c499989af09e6502a68e78f6b7110",
+    "url": "/react_demo_table/index.html"
+  },
+  {
+    "revision": "cf327243e1191fd2a027",
+    "url": "/react_demo_table/static/js/2.c0549c3e.chunk.js"
+  },
+  {
+    "revision": "83df5e53ab9e59dbd3cf",
+    "url": "/react_demo_table/static/js/main.fbb04c8c.chunk.js"
+  },
+  {
+    "revision": "f01461478ba2e556ba2e",
+    "url": "/react_demo_table/static/js/runtime~main.682f487e.js"
+  }
+]);
